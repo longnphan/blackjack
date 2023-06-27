4432e214ff479b0e3f730db11fa18b6f7673f956
Long Phan <longnphan@outlook.com>
2023-06-26 20:18:16 -0600

    add createCardDeck, shuffleCards fn's

--- js/index.js
+++ js/index.js
@@ -7,8 +7,35 @@ const dealBtn = document.querySelector(".btn-deal");
 const doubleDownBtn = document.querySelector(".btn-doubleDown");
 const splitBtn = document.querySelector(".btn-split");
 const standBtn = document.querySelector(".btn-stand");
+const cardShoe = [];
 let totalBalanceAmt = 1000;
 let totalBetAmt = 0;
+
+function createCardDeck() {
+  const cardSuit = ["clubs", "diamonds", "hearts", "spades"];
+  const cardValue = [
+    "ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "jack",
+    "queen",
+    "king",
+  ];
+
+  for (let suit of cardSuit) {
+    for (let val of cardValue) {
+      cardShoe.push(`${suit}_${val}`);
+    }
+  }
+  shuffleCards(cardShoe);
+}
 
 function calcBetAmt() {
   // Increment bet amount when chips are clicked on.
@@ -56,8 +83,20 @@ function disableChipBtns() {
   }
 }
 
-disableActionBtns();
-calcBetAmt();
+function shuffleCards(deck) {
+  // Durstenfeld Shuffle Algorithm
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+}
+
+// disableActionBtns();
+// calcBetAmt();
+
+// console.log("before card created", cardShoe);
+// createCardDeck();
+// console.log("After:", cardShoe);
 
 // Might use later
 // clearBet.removeAttribute("disabled");
